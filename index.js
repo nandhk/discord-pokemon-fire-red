3d82a7edb0e16a4a11f30eb7629783482472bb49
NotGubeesh <80934417+NotGubeesh@users.noreply.github.com>
2021-06-01 10:19:21 +0530

    something better

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const GameBoyAdvance = require('gbajs');
+const fs = require('fs');//import all modules required, if you get an error run npm i <missing module> in the shell
+const GameBoyAdvance = require('gbajs');//the module that helped to make this bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const gba = new GameBoyAdvance();
@@ -8,52 +8,9 @@ var biosBuf = fs.readFileSync('./node_modules/gbajs/resources/bios.bin');
 gba.setBios(biosBuf);
 gba.setCanvasMemory();
 const disbut = require('discord-buttons')(client);
-
-
-let Abutton = new disbut.MessageButton()
-    .setStyle('red')
-    .setLabel('A')
-    .setID('secret_gba_a_id');
-let Bbutton = new disbut.MessageButton()
-    .setStyle('green')
-    .setLabel('B')
-    .setID('secret_gba_b_id');
-let Up = new disbut.MessageButton()
-    .setStyle('gray')
-    .setLabel('Up')
-    .setID('secret_gba_up_id');
-let Down = new disbut.MessageButton()
-    .setStyle('gray')
-    .setLabel('Down')
-    .setID('secret_gba_down_id');
-let Right = new disbut.MessageButton()
-    .setStyle('gray')
-    .setLabel('Right')
-    .setID('secret_gba_right_id');
-let Left = new disbut.MessageButton()
-    .setStyle('gray')
-    .setLabel('Left')
-    .setID('secret_gba_left_id');
-let Select = new disbut.MessageButton()
-    .setStyle('blurple')
-    .setLabel('Select')
-    .setID('secret_gba_select_id');
-let Start = new disbut.MessageButton()
-    .setStyle('blurple')
-    .setLabel('Start')
-    .setID('secret_gba_start_id');
-let L = new disbut.MessageButton()
-    .setStyle('gray')
-    .setLabel('L')
-    .setID('secret_gba_l_id')
-let R = new disbut.MessageButton()
-    .setStyle('gray')
-    .setLabel('R')
-    .setID('secret_gba_r_id');
-
-
+const [Abutton,Bbutton,R,L,Start,Select,Left,Right,Up,Down] = require('./buttons.js')
+const config = require('./config.json')
 gba.logLevel = gba.LOG_ERROR;
-
 gba.loadRomFromFile('./game.gba', function(err, result) {
   if (err) {
     console.error('loadRom failed:', err);
@@ -63,20 +20,39 @@ gba.loadRomFromFile('./game.gba', function(err, result) {
 });
 const Game = gba.screenshot();
 const keypad = gba.keypad;
-
 client.on('ready', ready => {
   console.log('ready')
 })
-
+let admin = [config.devID, config.devID2];
+client.on('message',message => {
+  if (!admin.includes(message.author.id)) return;
+  if(message.author.bot) return;
+  if(!message.content.startsWith('$gba')) return;
+  if(message.content.startsWith('$gba'))
+   message.channel.send(`The GBA Developed By **${config.devName}** <a:Black_Fire:819183722573987840>\nControls - 🎮`,{buttons:[Abutton,Bbutton,Start,Select]})
+});
+client.on('message',message => {
+  if (!admin.includes(message.author.id)) return;
+  if(message.author.bot) return;
+  if(!message.content.startsWith('$gba')) return;
+  if(message.content.startsWith('$gba'))
+   message.channel.send('`Keys`',{buttons:[Up,Down,Left,Right]})
+});
+client.on('message',message => {
+  if (!admin.includes(message.author.id)) return;
+  if(message.author.bot) return;
+  if(!message.content.startsWith('$gba')) return;
+  if(message.content.startsWith('$gba'))
+   message.channel.send('`HELP`',{buttons:[L,R]})
+});
 
 var idx = 1;
-// keys - L , R , SELECT , START ,RIGHT , LEFT , DOWN , UP
+
 client.on('clickButton', async (button) => {
   if (button.id === 'secret_gba_a_id') {
      keypad.press(keypad.A)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 
@@ -85,7 +61,6 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.B)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 
@@ -94,7 +69,6 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.UP)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 client.on('clickButton', async (button) => {
@@ -102,7 +76,6 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.DOWN)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 client.on('clickButton', async (button) => {
@@ -110,7 +83,6 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.LEFT)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 client.on('clickButton', async (button) => {
@@ -118,7 +90,6 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.RIGHT)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-          button.defer()
   }
 });
 client.on('clickButton', async (button) => {
@@ -126,15 +97,13 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.SELECT)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 client.on('clickButton', async (button) => {
   if (button.id === 'secret_gba_start_id') {
      keypad.press(keypad.START)
-        await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
+        await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png')) 
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 client.on('clickButton', async (button) => {
@@ -142,7 +111,6 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.L)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-         button.defer()
   }
 });
 client.on('clickButton', async (button) => {
@@ -150,8 +118,7 @@ client.on('clickButton', async (button) => {
      keypad.press(keypad.R)
         await gba.screenshot().pack().pipe(fs.createWriteStream('gba' + idx + '.png'))
          await button.channel.send({files: ['./gba1.png']}).then(msg =>{setTimeout(() => msg.delete(), 800)})
-          await button.defer()
   }
 });
 
-client.login("Your Bot Token")
+client.login(config.Token)
